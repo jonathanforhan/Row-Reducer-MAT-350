bf1eb06c6d887dd147cdc8f03925ffa13181886b
jonathanforhan <jonathan.forhan@gmail.com>
2023-12-05 19:23:56 -0500

    ignore mistaken errors from ts-lint

--- src/scale.ts
+++ src/scale.ts
@@ -17,7 +17,9 @@ export default class Scale extends Inputfield {
       const new_rhs = this._mfe.getPromptValue("rhs");
       if (new_lhs !== new_rhs) {
         let newVal = new_lhs !== this._prev_R ? new_lhs : new_rhs;
+        // @ts-ignore
         this._mfe.setPromptValue("lhs", newVal, {});
+        // @ts-ignore
         this._mfe.setPromptValue("rhs", newVal, {});
         this._prev_R = newVal;
       }
